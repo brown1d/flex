7fd0261cc745b255eacb9d70f3d487335496fd5a
David Brown <david.brown@24i.com>
2023-02-26 00:56:00 +0100

    Use header to define class

--- fiw.cpp
+++ fiw.cpp
@@ -5,43 +5,34 @@
  * Frame Information Word class
  */
 
-#include "flex.h"
+#include "flex.hh"
+#include "fiw.hh"
 #include <iostream>
 #include <stdexcept>
 
 using namespace std;
-
-class FrameInformationWord {
-private:
-  uint32_t cycleNumber;
-  uint32_t frameNumber;
-  uint32_t repeatPaging;
-  uint32_t lowTrafficFlags;
+FrameInformationWord::FrameInformationWord(uint32_t cycleNumber, uint32_t frameNumber, uint32_t repeatPaging, uint32_t lowTrafficFlags) {
+  if (!(cycleNumber <= 14 && frameNumber <= 127 && repeatPaging <= 1)) {
+    throw std::invalid_argument("Frame Information Word: Parameters out of range");
+  }
   
-public: 
-  fiw(uint32_t cycleNumber, uint32_t frameNumber, uint32_t repeatPaging, uint32_t lowTrafficFlags) {
-    if (!(cycleNumber <= 14 && frameNumber <= 127 && repeatPaging <= 1)) {
-      throw std::invalid_argument("Frame Information Word: Parameters out of range");
-    }
-
-    this->cycleNumber = cycleNumber;
-    this->frameNumber = frameNumber;
-    this->repeatPaging = repeatPaging;
-    this->lowTrafficFlags = lowTrafficFlags;
+  this->cycleNumber = cycleNumber;
+  this->frameNumber = frameNumber;
+  this->repeatPaging = repeatPaging;
+  this->lowTrafficFlags = lowTrafficFlags;
   }
 
-  uint32_t getCodeword() {
-    uint32_t cw = 0x0;
-    cw |= this->cycleNumber << 4;
-    cw |= this->frameNumber << 8;
-    cw |= this->repeatPaging << 16;
+uint32_t FrameInformationWord::getCodeword() {
+  uint32_t cw = 0x0;
+  cw |= this->cycleNumber << 4;
+  cw |= this->frameNumber << 8;
+  cw |= this->repeatPaging << 16;
     cw |= this->lowTrafficFlags << 17;
-
+    
     cw = apply4bitChecksum(cw);
     cw = applyBchAndParity(cw);
     
     return cw;
-  }
 }
   
   
