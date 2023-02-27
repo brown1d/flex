877b76d184180ea4e3f6584984f2c87dcf7878f7
David Brown <david.brown@24i.com>
2023-02-27 18:14:31 +0100

    Add in BIW1

--- codewords.cpp
+++ codewords.cpp
@@ -9,15 +9,15 @@
 #include "codewords.hh"
 
 // BIW1 Constructor
-Biw1::Biw1(uint32_t priorityAddress, uint32_t endOfBlock, uint32_t vectorFieldStart, uint32_t carryOn, uint32_t frameIdCollapseMark) {
+BIW1::BIW1(uint32_t priorityAddress, uint32_t endOfBlock, uint32_t vectorFieldStart, uint32_t carryOn, uint32_t frameIdCollapseMark) {
   this->priorityAddress = priorityAddress & 0xf;
   this->endOfBlock = endOfBlock & 0x3;
   this->vectorFieldStart = vectorFieldStart & 0x3f;
   this->carryOn = carryOn & 0x3;
-  this->fieldIdCollapseMark = frameIdCollapseMark & 0x7;
+  this->frameIdCollapseMark = frameIdCollapseMark & 0x7;
 }
 
-uint32_t Biw1::getCodeword() {
+uint32_t BIW1::getCodeword() {
   uint32_t cw = 0x0;
 
   cw |= this->priorityAddress << 4;
