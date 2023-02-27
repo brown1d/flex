d899f981b9b0ca022ffd01b7d32e62300d6a7bb6
David Brown <david.brown@24i.com>
2023-02-27 18:14:16 +0100

    Add in BIW1

--- codewords.hh
+++ codewords.hh
@@ -9,17 +9,17 @@
 #ifndef CODEWORDS_H
 #define CODEWORDS_H
 
-class Biw1 {
+class BIW1 {
 private:
   uint32_t priorityAddress;
   uint32_t endOfBlock;
   uint32_t vectorFieldStart;
   uint32_t carryOn;
   uint32_t frameIdCollapseMark;
-
+  
 public:
-  Biw1(uint32_t priorityAddress, uint32_t endOfBlock, uint32_t vectorFieldStart, uint32_t carryOn, uint32_t frameIdCollapseMark);
+  BIW1(uint32_t priorityAddress, uint32_t endOfBlock, uint32_t vectorFieldStart, uint32_t carryOn, uint32_t frameIdCollapseMark);
   uint32_t getCodeword();
-}
+};
   
 #endif
