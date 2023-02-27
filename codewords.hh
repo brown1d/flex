0952c64061e1356cdee128233a70846de0f8f411
David Brown <david.brown@24i.com>
2023-02-27 18:30:40 +0100

    Complete out with BIW3 and BIW4

--- codewords.hh
+++ codewords.hh
@@ -21,5 +21,47 @@ public:
   BIW1(uint32_t priorityAddress, uint32_t endOfBlock, uint32_t vectorFieldStart, uint32_t carryOn, uint32_t frameIdCollapseMark);
   uint32_t getCodeword();
 };
+
+class BIW2 {
+private:
+  uint32_t localId;
+  uint32_t timezone;
   
+public:
+  BIW2(uint32_t localId, uint32_t timezone);
+  uint32_t getCodeword();
+};
+
+class BIW3 {
+private:
+  uint32_t year;
+  uint32_t month;
+  uint32_t day;
+  
+public:
+  BIW3(uint32_t day, uint32_t month, uint32_t year);
+  uint32_t getCodeword();
+
+private:
+  bool checkDay(uint32_t day);
+  bool checkMonth(uint32_t month);
+  bool checkYear(uint32_t year);
+};
+
+class BIW4 {
+private:
+  uint32_t hour;
+  uint32_t minute;
+  uint32_t second;
+
+public:
+  BIW4(uint32_t hour, uint32_t minute, uint32_t second);
+  uint32_t getCodeword();
+
+private:
+  bool checkHour(uint32_t hour);
+  bool checkMinute(uint32_t minute);
+  bool checkSecond(uint32_t second);
+};
+
 #endif
